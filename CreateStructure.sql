7c91807a32e9eabeacfc2f8f225a90f3d79a8f21
natalieli13 <lns_home@mail.ru>
2023-08-03 21:54:26 +0300

    Создание таблиц

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -1,1 +1,21 @@
+create table SKU (
+    ID int identity primary key,
+    Code as ('s' + cast(ID as varchar)) unique,
+    [Name] varchar(255)
+);
 
+create table Family  (
+    ID int identity primary key,
+    SurName varchar(255),
+    BudgetValue int
+);
+
+create table Basket (
+    ID int identity primary key,
+    ID_SKU int foreign key references SKU(ID),
+    ID_Family int foreign key references Family(ID),
+    Quantity int check (Quantity >= 0),
+    Value int check (Value >= 0),
+    PurchaseDate datetime default GETDATE(),
+    DiscountValue int
+);
